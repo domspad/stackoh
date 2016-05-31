5778b8568714a3d23c9ec2fa23a00a20672f4a53
Lucas Kushner <lphk92@gmail.com>
2016-05-31 23:37:55 +0000

    Added plotting capability to client

--- stackoh/server.py
+++ stackoh/server.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/")
 def hello():
     return render_template("index.html")
 
+@socketio.on("tags_request")
+def tags_request(message):
+    print "Received tags_request: ", message
+
+    # Call into SO API
+    data = dict()
+    data["test_tag"] = 1234
+
+    socketio.emit("tags_data", data)
+
 if __name__ == "__main__":
     import os
 
-    app.run(host=os.environ['IP'], port=int(os.environ['PORT']), debug=True)
+    socketio.run(app, host=os.environ['IP'], port=int(os.environ['PORT']), debug=True)
