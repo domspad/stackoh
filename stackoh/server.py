3508bb6718f835178f2bc75b548b128a6d4c3541
domspad <domspad@umich.edu>
2016-06-03 18:47:30 -0700

    mp code

--- stackoh/server.py
+++ stackoh/server.py
@@ -3,10 +3,16 @@ from flask_socketio import SocketIO
 
 from data_utils import tag_question_count
 
+import websocket
+from multiprocessing import Pool, TimeoutError
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 clients = list()
+
+#ws = websocket.create_connection("ws://qa.sockets.stackexchange.com/")
+#ws.send("155-questions-active")
 
 @app.route("/")
 def hello():
@@ -32,9 +38,30 @@ def tags_request(message):
         for t in tags:
             data[t] = tag_question_count(t)
 
+        print "Sending tags_data"
         socketio.emit("tags_data", data, room=request.sid)
+
+def wait_and_do_magic():
+    print "Start the magic!"
+    import time
+    time.sleep(3)
+    return str(time.time())
+    #return ws.recv()
+
+def magic_callback(str_data):
+    print "Sending update to clients!", str_data
+    socketio.emit("so_update", str_data, broadcast=True)
+    print "Successfully emitted data!"
+    pool.apply_async(wait_and_do_magic, callback=magic_callback)
+
+pool = Pool()
+
+#@app.before_first_request
+#def start_magic():
+#    pool.apply_async(wait_and_do_magic, callback=magic_callback)
 
 if __name__ == "__main__":
     import os
 
-    socketio.run(app, host=os.environ['IP'], port=int(os.environ['PORT']), debug=True)
+    pool.apply_async(wait_and_do_magic, callback=magic_callback)
+    socketio.run(app)#, host=os.environ['IP'], port=int(os.environ['PORT']))#, debug=True)
